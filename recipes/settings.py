41ed0f4f3359f82ead5d374ca3994094f9f550e5
Nandini-Kamireddy <nandinikamireddy11@gmail.com>
2025-02-10 22:26:42 +0530

    Configure MySQL database settings and add CORS middleware

--- recipes/settings.py
+++ recipes/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'veg','nonveg','desserts','snacks','drinks','tiffins',
-    'whitenoise',
+    'whitenoise','corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -55,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
@@ -85,10 +88,15 @@ WSGI_APPLICATION = 'recipes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'ekEappkQytQxPEXSLPMFbFQMgcHXRnat',
+        'HOST': 'autorack.proxy.rlwy.net',
+        'PORT': '13729',
     }
 }
+
 
 
 # Password validation
